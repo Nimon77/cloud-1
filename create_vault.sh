3d87a8ca4af05e33e41ac94256ab2cedcc1a88ed
nimon <nimon@42paris.fr>
2022-12-14 18:56:07 +0100

    Add auto TLS, ACME, CNAME, Cloudflare

--- create_vault.sh
+++ create_vault.sh
@@ -23,11 +23,21 @@ echo -n "Enter wordpress_admin_password: "
 read -s wordpress_admin_password
 echo ""
 
+echo -n "Enter cloudflare_global_token: "
+read -s cloudflare_global_token
+echo ""
+
+echo -n "Enter cloudflare_token: "
+read -s cloudflare_token
+echo ""
+
 echo "Creating vault file..."
 cat << EOF > vault
 ---
 vault_database_password: $database_password
 vault_wordpress_admin_password: $wordpress_admin_password
+vault_cloudflare_global_token: $cloudflare_global_token
+vault_cloudflare_token: $cloudflare_token
 EOF
 
 echo "Encrypting vault file..."
